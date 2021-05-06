ceb2f788b094aded610a5000229ffab516eb2b46
Avihai.Adler <avihai.adler@s.afeka.ac.il>
2021-05-06 16:34:44 +0000

    src/main/java/twins/operations/OperationHandler.java edited online with Bitbucket

--- src/main/java/twins/operations/OperationHandler.java
+++
@@ -1,8 +0,0 @@
-package twins.operations;
-
-import twins.data.ItemEntity;
-import twins.data.UserEntity;
-
-public interface OperationHandler {
-	public void handle(String operationType, UserEntity user, ItemEntity item);
-}
